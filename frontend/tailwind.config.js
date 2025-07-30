d57d43096add09d66c109d4b181726114b85ef4a
Mostafa Kermaninia <mkermani1383@gmail.com>
2025-07-30 21:22:12 +0000

    Finish merging stashed changes after pull

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}", // این خط مهم‌ترین بخش است
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
