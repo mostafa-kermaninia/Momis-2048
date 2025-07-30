10ee85464be527255db6eefbc31a6a7ff0f71bb1
mosiyo <mkermani1383@gmail.com>
2025-07-31 01:10:18 +0330

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css'; // ما استایل‌های اصلی را اینجا وارد می‌کنیم
+import './main.css'; // این خط صحیح است
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
