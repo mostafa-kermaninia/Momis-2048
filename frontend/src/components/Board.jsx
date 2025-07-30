2a0cde34c9bc751bb2000dcf9d336e4bc2411678
mosiyo <mkermani1383@gmail.com>
2025-07-30 03:46:17 +0330

    Board was big!

--- frontend/src/components/Board.jsx
+++ frontend/src/components/Board.jsx
@@ -1,26 +1,31 @@
 import React from 'react';
+
+// کامپوننت Tile نیازی به تغییر ندارد و همان کد قبلی صحیح است.
 import Tile from './Tile';
 
 const Board = ({ tiles }) => {
   return (
-    <div className="board-container">
+    <div className="board-container-2048">
       {/* لایه اول: گرید ثابت پس‌زمینه */}
-      <div className="grid-background">
+      <div className="grid-background-2048">
         {Array(16).fill(null).map((_, i) => (
-          <div key={i} className="grid-cell" />
+          <div key={i} className="grid-cell-2048" />
         ))}
       </div>
 
       {/* لایه دوم: کاشی‌های متحرک */}
-      <div className="tile-container">
+      <div className="tile-container-2048">
         {tiles.map(tile => {
-          // استایل موقعیت‌یابی برای هر کاشی
+          // استایل موقعیت‌یابی واکنش‌گرا برای هر کاشی
           const style = {
-            transform: `translate(calc(${tile.col} * (var(--cell-size) + var(--cell-gap))), calc(${tile.row} * (var(--cell-size) + var(--cell-gap))))`
+            transform: `translate(
+              calc(${tile.col} * (var(--cell-size) + var(--cell-gap))), 
+              calc(${tile.row} * (var(--cell-size) + var(--cell-gap)))
+            )`
           };
           
           return (
-            <div key={tile.id} className="tile-wrapper" style={style}>
+            <div key={tile.id} className="tile-wrapper-2048" style={style}>
               <Tile 
                 value={tile.value}
                 isNew={tile.isNew}
