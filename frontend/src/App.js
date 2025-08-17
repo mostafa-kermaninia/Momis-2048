b4283a730126a797137eb19d33e075410b5ba35f
AmirNaddaf2004 <amir.naddaf@ut.ac.ir>
2025-08-17 23:53:50 +0330

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -571,7 +571,7 @@ function App() {
                             <div className="flex flex-col items-center gap-6 w-full max-w-md text-center">
                                 <Game2048
                                     onGameOver={handleGameOver}
-                                    onSaveMoves={saveScenario}
+                                    
                                     onGoHome={handleGoHome}
                                     initialBestScore={bestScore}
                                     isMuted={isMuted}
