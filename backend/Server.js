e0011c984438aa2ff06711df76b848dd630720e4
AmirNaddaf2004 <amir.naddaf@ut.ac.ir>
2025-08-26 14:46:55 +0330

    Update Server.js

--- backend/Server.js
+++ backend/Server.js
@@ -66,7 +66,7 @@ async function getActiveReferredFriendsCount(currentUserId) {
             },
             include: [{
                 model: Score,
-                as: 'Scores', // از alias 'Scores' که در db.js تعریف شده، استفاده می‌کنیم
+                as: 'scores', // از alias 'Scores' که در db.js تعریف شده، استفاده می‌کنیم
                 attributes: [], // نیازی به واکشی فیلدهای Score نیست، فقط برای شرط join استفاده می‌شود
                 required: true // این شرط تضمین می‌کند که کاربر حداقل یک Score داشته باشد
             }],
